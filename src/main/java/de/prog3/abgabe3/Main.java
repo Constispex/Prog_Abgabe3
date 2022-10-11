5f374b91f55224976a97d7bde8131faef2ffc709
Fenris22127 <elisa.woelk@outlook.de>
2022-10-11 09:04:24 +0200

    Added ArraySort class

--- src/main/java/de/prog3/abgabe3/Main.java
+++ src/main/java/de/prog3/abgabe3/Main.java
@@ -2,6 +2,7 @@ package de.prog3.abgabe3;
 
 public class Main {
     public static void main(String[] args) {
+        SortArray sort = new SortArray(10);
         System.out.println("Hello world!");
     }
 }
---
+++ src/main/java/de/prog3/abgabe3/SortArray.java
@@ -0,0 +1,7 @@
+package de.prog3.abgabe3;
+
+public class SortArray<T extends Comparable<T>> {
+    public SortArray(int length) {
+
+    }
+}

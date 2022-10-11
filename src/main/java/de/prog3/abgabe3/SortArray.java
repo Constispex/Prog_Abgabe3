e3dc7b40922a60186ea168a10ba60614bf78d6b9
Fenris22127 <elisa.woelk@outlook.de>
2022-10-11 09:08:09 +0200

    Added ArraySort class content

--- src/main/java/de/prog3/abgabe3/SortArray.java
+++ src/main/java/de/prog3/abgabe3/SortArray.java
@@ -1,7 +1,13 @@
 package de.prog3.abgabe3;
 
+import java.util.Random;
+
 public class SortArray<T extends Comparable<T>> {
     public SortArray(int length) {
-
+        T[] arr = new T[length];
+        Random r = new Random();
+        for (int i = 0; i == length; i++) {
+            arr[i] = r.nextInt();
+        }
     }
 }

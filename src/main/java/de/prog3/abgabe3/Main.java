b5b8258c8829fa5e0bc3315bdf550e83d3a646ce
Elisa <elisa-johanna.woelk@outlook.de>
2022-10-15 17:39:35 +0200

    Added MergeSort that may or may not work

--- src/main/java/de/prog3/abgabe3/Main.java
+++ src/main/java/de/prog3/abgabe3/Main.java
@@ -1,5 +1,6 @@
 package de.prog3.abgabe3;
 
+import java.util.Comparator;
 import java.util.Random;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
@@ -8,14 +9,14 @@ public class Main {
     public static void main(String[] args) {
         final int ARR_SIZE = 10;
         final int nThreads = Runtime.getRuntime().availableProcessors();
-        final int[] arr = createArray(ARR_SIZE);
-
+        final Integer[] arr = createArray(ARR_SIZE);
         ForkJoinPool exec = new ForkJoinPool(nThreads);
-        exec.invoke(new SortArray(arr, 10));
+
+        exec.invoke(new SortArray<Integer>(arr, 0, 10, Integer::compareTo));
     }
 
-    private static int[] createArray(int length) {
-        int[] arr = new int[length];
+    private static Integer[] createArray(int length) {
+        Integer[] arr = new Integer[length];
         Random r = new Random();
         for (int i = 0; i < length; i++) {
             arr[i] = r.nextInt(0, 100);
--- src/main/java/de/prog3/abgabe3/SortArray.java
+++ src/main/java/de/prog3/abgabe3/SortArray.java
@@ -1,24 +1,83 @@
 package de.prog3.abgabe3;
 
-import java.lang.reflect.Array;
-import java.util.Random;
-import java.util.concurrent.RecursiveAction;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.concurrent.RecursiveTask;
 
-public class SortArray <T extends Comparable<T>> extends RecursiveAction {
-    private static final int CHUNKSIZE = 20;
-    private final int[] u;
-    private final int length;
+public class SortArray<T extends Comparable<T>> extends RecursiveTask {
+    final int switchSort = 10;
+    private final int start;
+    private final int end;
+    private final T[] arr;
+    private final T[] leftArray;
+    private final T[] rightArray;
+    private final Comparator<T> comp;
 
-    public SortArray(int[] u, int length) {
-        this.length = length;
-        this.u = u;
+    SortArray(T[] arr, int start, int end, Comparator<T> c) {
+        this.start = start;
+        this.end = end;
+        this.arr = arr;
+        leftArray = Arrays.copyOfRange(arr, start, arr.length / 2);
+        rightArray = Arrays.copyOfRange(arr, (arr.length / 2) + 1, end);
+        comp = c;
     }
-    public void sortArr(T[] arr) {
 
+    private <T> void merge(T[] arr, int start, int mid, int end, Comparator<T> comp) {
+        // Find sizes of two subarrays to be merged
+        int n1 = mid - start + 1;
+        int n2 = end - mid;
+
+        /* Merge temp arrays */
+        int i = 0, j = 0;
+
+        int k = start;
+        while (i < n1 && j < n2) {
+            if (leftArray[i].compareTo(rightArray[j]) != 1) {
+                arr[k] = (T) leftArray[i];
+                i++;
+            } else {
+                arr[k] = (T) rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = (T) leftArray[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = (T) rightArray[j];
+            j++;
+            k++;
+        }
     }
 
     @Override
-    protected void compute() {
+    protected Object compute() {
+        if (start - end <= switchSort) {
+            //insertion sort
+        }
+        else {
+            int mid = (start + end) / 2;
+            SortArray<T> right = new SortArray<>(arr, start, mid, comp);
+            SortArray<T> left = new SortArray<>(arr, mid + 1, end, comp);
 
+            right.fork();
+            left.compute();
+            right.join();
+
+            merge(arr, start, mid, end, comp);
+        }
+        return null;
+    }
+
+    public int compare(Object a, Object b) {
+        return Integer.compare((Integer) a, (Integer) b);
     }
 }
+
